63c1b28efb258aaf3b56716f026c6e845c185fd3
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-06 08:08:19 +0200

    Some querying...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import rdf from 'rdflib';
+import rdf, { Namespace } from 'rdflib';
 
 import './App.css';
+
+const props = Namespace('http://www.wikidata.org/prop/');
+const wd = Namespace('http://www.wikidata.org/entity/');
 
 class App extends Component {
 
   componentDidMount() {
-    axios.get(`${process.env.PUBLIC_URL}/themeparks.rdf`).then(response => {
-      // console.log(response.data);
+    axios.get(`${process.env.PUBLIC_URL}/themeparks.ttl`).then(response => {
+
+      // Create the store and load the RDF
       const store = rdf.graph();
-      rdf.parse(response.data, store, 'http://foo.bar', 'text/turtle');
-      console.log(store.toNT());
+      rdf.parse(response.data, store, 'http://localhost/#', 'text/turtle');
+      
+      // console.log(store.toNT());
+
+      // Match every triple "* instanceOf amusementPark"
+      const ids = store.match(null, props('P31'), wd('Q194195'));
+      const parks = ids.map(id => {
+        return store.connectedStatements(id.subject);
+      });
+
+      console.log(parks);
     });
   }
 
